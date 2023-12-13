6d1120b4c4a028a42d2c21014d940dac464fad9d
Oscar <panecillopeter@gmail.com>
2023-12-13 12:13:45 +0100

    acabado pagina calendario, igual mirar responsive

--- js/app.js
+++ js/app.js
@@ -1,69 +1,146 @@
+var tooltip_en_pantalla = false;
+
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
+    var eventos = null;
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        defaultView: 'dayGridMonth',
-        locale: 'es',
-        buttonText: {
-            today: 'Hoy',
-            month: 'Mes',
-            week: 'Semana',
-            day: 'Día',
-            list: 'Lista'
-        },
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,listMonth'
-        },
+    // Fetch events from the JSON file
+    fetch('https://toimil.github.io/AtletismoSantiago/data/calendario_fga.json')
+        .then(response => response.json())
+        .then(data => {
+            eventos = data;
 
-        eventMouseEnter: function (info) {
-            $(info.el).tooltip({
-                title: info.event.title,
-                placement: 'top',
-                trigger: 'hover',
-                container: 'body'
+            // Create the FullCalendar instance after fetching the data
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                height: 'auto',
+                defaultView: 'dayGridMonth',
+                locale: 'es',
+                eventLimit: true,
+                dayMaxEvents: 3,
+                views: {
+                    week: {
+                        dayMaxEvents: 15
+                    }
+                },
+
+                buttonText: {
+                    today: 'Hoy',
+                    month: 'Mes',
+                    week: 'Semana',
+                    day: 'Día',
+                    list: 'Lista'
+                },
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,listMonth'
+                },
+
+                events: eventos,
+                eventClick: function (info) {
+                    info.jsEvent.preventDefault();
+
+                    // Cerrar tooltip existente si está en pantalla
+                    if (tooltip_en_pantalla) {
+                        $('.tooltip.bs-tooltip-auto.fade.show').remove();
+                        tooltip_en_pantalla = false;
+                    }
+
+                    $(info.el).tooltip({
+                        title: function () {
+                            var tooltipContent = '<strong>' + info.event.title + '</strong>';
+
+                            if (info.event.extendedProps.place) {
+                                tooltipContent += '<br>Lugar: ' + info.event.extendedProps.place;
+                            }
+
+                            if (info.event.url) {
+                                tooltipContent += '<br><a href="' + info.event.url + '" target="_blank">Más información</a>';
+                            }
+
+                            return tooltipContent;
+                        },
+                        placement: 'top',
+                        trigger: 'manual',  // Desactivar el cierre automático
+                        container: 'body',
+                        html: true,
+                        template: '<div id="tooltip_abierto" style="display: block !important" class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
+                    });
+
+                    $(info.el).tooltip('show');
+                    tooltip_en_pantalla = true;
+                },
+
+                eventMouseEnter: function (info) {
+                    $(info.el).tooltip({
+                        title: function () {
+                            var tooltipContent = '<strong>' + info.event.title + '</strong>';
+
+                            if (info.event.extendedProps.place) {
+                                tooltipContent += '<br>Lugar: ' + info.event.extendedProps.place;
+                            }
+
+                            if (info.event.url) {
+                                tooltipContent += '<br><a href="' + info.event.url + '" target="_blank">Más información</a>';
+                            }
+
+                            return tooltipContent;
+                        },
+                        placement: 'top',
+                        trigger: 'manual',  // Desactivar el cierre automático
+                        container: 'body',
+                        html: true,
+                        template: '<div class="tooltip" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>'
+
+                    });
+
+                    $(info.el).tooltip('show');
+
+
+                    // Evento para ocultar el tooltip cuando el mouse sale del evento
+                    $(info.el).on('mouseleave', function () {
+                        setTimeout(function () {
+                            if (!$('.tooltip:hover').length) {
+                                $(info.el).tooltip('hide');
+                            }
+                        }, 100);
+                    });
+
+                    // Evento para mantener el tooltip visible cuando el mouse está sobre él
+                    $('.tooltip').on('mouseleave', function () {
+                        $(info.el).tooltip('hide');
+                    });
+                },
             });
-        },
 
-        resourceLabelText: 'Rooms',
-        resources: 'https://fullcalendar.io/demo-resources.json?with-nesting&with-colors',
-        events: [
-            {
-                title: 'All Day Event',
-                description: 'description for All Day Event',
-                start: '2023-12-01'
-            },
-            {
-                title: 'Click for Google',
-                description: 'description for Click for Google',
-                url: 'http://google.com/',
-                start: '2023-12-13'
-            }
-        ]
-    });
-    calendar.render();
-    
+            // Render the calendar
+            calendar.render();
+
+           
+
+        })
+        .catch(error => {
+            console.error('Error fetching events:', error);
+        });
 });
 
-  
 
-$(document).ready(function(){
+$(document).ready(function () {
     //HERO SLIDER
     $('#hero-slider').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        items:1,
-        dots:false,
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
         autoplay: true,
         autoplayTimeout: 4000,
-        smartSpeed:1000,
-        navText : ['<','>'],
+        smartSpeed: 1000,
+        navText: ['<', '>'],
         responsive: {
             0: {
                 responsiveClass: true,
-                center:true,
+                center: true,
                 items: 1,
                 nav: false,
                 dots: true
@@ -85,49 +162,49 @@ $(document).ready(function(){
     })
     //Projcet-slider
     $('#project-slider').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:false,
-        dots:true,
-        smartSpeed:1000,
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        smartSpeed: 1000,
         autoplay: true,
         autoplayTimeout: 4000,
-        margin:24,
-        responsive:{
-            0:{
-                items:1,
-                margin:0,
+        margin: 24,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0,
             },
-            768:{
-               items:2
+            768: {
+                items: 2
             },
-            1140:{
-               items:2,
-               center:true
+            1140: {
+                items: 2,
+                center: true
             }
 
-         
+
         }
     })
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        dots:true,
-        items:1,
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        items: 1,
         autoplay: true,
         autoplayTimeout: 4000,
-        smartSpeed:2000,
-        responsive:{
-            0:{
-                dots:false,
+        smartSpeed: 2000,
+        responsive: {
+            0: {
+                dots: false,
             },
-            768:{
-                dots:true,
+            768: {
+                dots: true,
             },
-            1140:{
-              
+            1140: {
+
             }
         }
     })
